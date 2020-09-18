f9f94bb97bdb92b1b549b8c5d4570a62c0a364f9
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-19 00:25:56 +0530

    removed unneccesary node_mudules inside spikes

---
+++ spikes/googleOauth/googleAuth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=555909970408-67qftpqa4ms2mmf81eucrob6c6fn5otv.apps.googleusercontent.com&response_type=code&redirect_uri=http://localhost:8000/callback&scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly`;
+  res.redirect(googleUrl);
+});
+
+app.get('/callback', (req, res) => {
+  console.log(req.query.code);
+  res.end();
+});
+
+app.listen(8000, () => console.log('listening'));
