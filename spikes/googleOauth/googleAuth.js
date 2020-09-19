d033e3ae44fa275f9e98984ed384843d7b146100
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-19 15:19:16 +0530

    modified gitignore

--- spikes/googleOauth/googleAuth.js
+++ spikes/googleOauth/googleAuth.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=555909970408-67qftpqa4ms2mmf81eucrob6c6fn5otv.apps.googleusercontent.com&response_type=code&redirect_uri=http://localhost:8000/callback&scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly`;
+  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=555909970408-67qftpqa4ms2mmf81eucrob6c6fn5otv.apps.googleusercontent.com&response_type=code&&scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly`;
   res.redirect(googleUrl);
 });
 
