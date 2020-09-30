c576eca160b8aefb8cca867a27d36aa350bdd596
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-30 20:32:40 +0530

    added travis.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,9 @@
-#! /bin/bash
-
-rm -rf KJ-react
-echo 'cloning'
-git clone  https://${GITHUB_TOKEN}:x-oauth-basic@github.com/photongupta/KJ-react.git 2> /dev/null
-cd KJ-react
-echo 'installing'
-npm install 2> /dev/null
-npm test
-
-
-cd ..
-rm -rf Knowledge-Junction
-echo 'cloning'
-git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/photongupta/Knowledge-Junction.git 2> /dev/null
+git clone git@github.com:photongupta/Knowledge-Junction.git Knowledge-Junction
 cd Knowledge-Junction
-echo 'installing'
 npm install 2> /dev/null
 npm test
-
-echo 'creating build'
 npm run build 2> /dev/null
-mkdir -p ../public
-mv build/* ../public/.
-cd ../KJ-react
-rm .travis.yml
+cp -r ./build/* ../public
 cd ..
+npm install 2> /dev/null
 rm -rf Knowledge-Junction
-mv ${SERVER}/* ${SERVER}/.* .
-rm -rf ${SERVER}
-echo 'created build' 
