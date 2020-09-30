fd3328a910627d45a8a4ed1aa5ddbcf42b979524
Rashmi Gupta <rashmi224141@gmail.com>
2020-10-01 00:05:26 +0530

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,23 @@
+rm -rf backend
+
 echo "cloning backend";
-git clone https://github.com/photongupta/KJ-react.git backend 2> /dev/null
+git clone https://github.com/photongupta/KW-react.git backend 2> /dev/null
 cd backend
 echo 'installing'
 npm install 2> /dev/null
 npm test
 cd ..
 
+rm -rf frontend
 echo 'cloning frontend'
-git clone https://github.com/photongupta/Knowledge-Junction.git frontend2> /dev/null
+git clone https://github.com/photongupta/Knowledge-Junction.git frontend 2> /dev/null
 cd frontend
 echo 'installing'
 npm install 2> /dev/null
 npm test
 echo 'creating build'
 npm run build 2> /dev/null
-mv ./build/* ../backend/public
+mv ./build/* ../backend/public/
 cd ..
 
 mv backend/* ./
