8e19424327c1e47aecdee31098a9995204add0e3
Rashmi Gupta <rashmi224141@gmail.com>
2020-10-02 22:23:38 +0530

    modified header component

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,6 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import Menubar from './Menubar';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: calc(100% - 8.5%);
+`;
+
 const Logo = styled.img`
   height: 48px;
   margin: 2px 15px;
@@ -9,10 +16,10 @@ const Logo = styled.img`
 
 const Header = function (props) {
   return (
-    <div className="header">
+    <Container>
       <Logo src="/images/logo-icon.png" alt="logo" />
       <Menubar onLogout={props.onLogout} topics={props.topics} />
-    </div>
+    </Container>
   );
 };
 
