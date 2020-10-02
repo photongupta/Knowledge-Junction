60dc07df76a6adf540a23fcbae5bb74904b4b867
Rashmi Gupta <rashmi224141@gmail.com>
2020-10-02 12:49:56 +0530

    made component of index.js styled

--- src/component/Index.js
+++ src/component/Index.js
@@ -14,18 +14,42 @@ const Container = styled.div`
   border: 1px solid #ccc;
 `;
 
+const Logo = styled.img`
+  height: 300px;
+  margin: 25px;
+`;
+
+const LoginButton = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+  box-shadow: 0 2px 2px 0 #aaa;
+  width: 280px;
+  height: 53px;
+  border-radius: 4px;
+  &:hover {
+    box-shadow: 2px 4px 8px 0 #aaa;
+  }
+  & > * {
+    margin-right: 10px;
+  }
+`;
+
+const GoogleIcon = styled.img`
+  height: 35px;
+  margin-left: 15px;
+`;
+
 const Index = function (props) {
   return (
     <Container>
-      <img alt="logo" src="/images/logo.png" className="logo-img" />
-      <a className="Oauth-link-container" href="/login">
-        <img
-          src="/images/google.png"
-          alt="google-icon"
-          className="google icon"
-        />
-        <div className="Oauth-link">Log in with Google</div>
-      </a>
+      <Logo alt="logo" src="/images/logo.png" />
+      <LoginButton href="/login">
+        <GoogleIcon src="/images/google.png" alt="google-icon" />
+        Log in with Google
+      </LoginButton>
     </Container>
   );
 };
