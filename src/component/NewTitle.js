25cf250db2ec30149af102283e1541a200850fb7
Rashmi Gupta <rashmi224141@gmail.com>
2020-10-02 22:51:26 +0530

    removed popup.css

--- src/component/NewTitle.js
+++ src/component/NewTitle.js
@@ -1,6 +1,45 @@
 import React, {useState} from 'react';
 import '../popup.css';
 import {useHistory} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 18px 30px;
+  background: white;
+  border-radius: 5px;
+  width: 20%;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CloseButton = styled.span`
+  position: relative;
+  left: 100%;
+  font-size: 15px;
+  color: #333;
+  cursor: pointer;
+  font-weight: 600;
+  &:hover {
+    color: red;
+  }
+`;
+
+const SubmitButton = styled.button`
+  height: 30px;
+  width: 60px;
+  margin-top: 15px;
+`;
 
 const NewTitle = function (props) {
   const history = useHistory();
@@ -8,26 +47,21 @@ const NewTitle = function (props) {
 
   const handleChange = (e) => setValue(e.target.value);
 
+  const updateHistory = () => history.push('/');
+
   const handleSubmit = function (e) {
     e.preventDefault();
     if (value) {
       props.onTitle(value);
       setValue('');
     }
-    history.push('/');
+    updateHistory();
   };
 
   return (
-    <div className="overlay">
-      <div className="popup">
-        <div
-          className="close"
-          onClick={() => {
-            history.push('/');
-          }}
-        >
-          X
-        </div>
+    <Overlay>
+      <Container>
+        <CloseButton onClick={updateHistory}>X</CloseButton>
         <form onSubmit={handleSubmit}>
           <input
             className="new-title"
@@ -39,10 +73,10 @@ const NewTitle = function (props) {
             autoFocus
           />
           <br />
-          <button type="submit">Add</button>
+          <SubmitButton type="submit">Add</SubmitButton>
         </form>
-      </div>
-    </div>
+      </Container>
+    </Overlay>
   );
 };
 
