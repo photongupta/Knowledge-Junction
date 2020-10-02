8adf4127b697eab5d338ad26e672d15ce73920e3
Rashmi Gupta <rashmi224141@gmail.com>
2020-10-02 12:28:40 +0530

    added styled div in Index.js

--- src/component/Index.js
+++ src/component/Index.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  top: 154px;
+  width: 689px;
+  height: 490px;
+  left: 373px;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+`;
 
 const Index = function (props) {
   return (
-    <div>
-      <div className="container">
-        <img alt="logo" src="/images/logo.png" className="logo-img" />
-        <a className="Oauth-link-container" href="/login">
-          <img
-            src="/images/google.png"
-            alt="google-icon"
-            className="google icon"
-          />
-          <div className="Oauth-link">Log in with Google</div>
-        </a>
-      </div>
-    </div>
+    <Container>
+      <img alt="logo" src="/images/logo.png" className="logo-img" />
+      <a className="Oauth-link-container" href="/login">
+        <img
+          src="/images/google.png"
+          alt="google-icon"
+          className="google icon"
+        />
+        <div className="Oauth-link">Log in with Google</div>
+      </a>
+    </Container>
   );
 };
 export default Index;
